ba355f7607f0240f8680bcc30b28debf3c0f6fcb
olafxiong <olafxiong@tencent.com>
2022-10-21 16:26:52 +0800

    example(cmake-cpp): avx

--- ml-ai/tensorflow/print-saved-model.py
+++ ml-ai/tensorflow/print-saved-model.py
@@ -4,7 +4,7 @@ import subprocess
 from tensorflow.core.protobuf.saved_model_pb2 import SavedModel
 
 
-def main():
+def print_saved_model():
     saved_model = SavedModel()
     with open(os.path.join(sys.argv[1], "saved_model.pb"), "rb") as f:
         saved_model.ParseFromString(f.read())
@@ -24,5 +24,17 @@ def main():
     subprocess.run(["saved_model_cli", "show", "--all", "--dir", sys.argv[1]])
 
 
+def print_saved_model_verbose():
+    saved_model = SavedModel()
+    with open(os.path.join(sys.argv[1], "saved_model.pb"), "rb") as f:
+        saved_model.ParseFromString(f.read())
+    # Iterate over every metagraph in case there is more than one
+    for meta_graph in saved_model.meta_graphs:
+        # Add operations in the graph definition
+        for node in meta_graph.graph_def.node:
+            print(node)
+            print("")
+
+
 if __name__ == "__main__":
-    main()
+    print_saved_model_verbose()
