38e2d65803de2ad3a4346c2e1740f17a1c057c63
olafxiong <olafxiong@tencent.com>
2022-11-23 14:50:18 +0800

    example(cmake-cpp): check template type

--- ml-ai/onnx/run-model.py
+++ ml-ai/onnx/run-model.py
@@ -1,34 +1,70 @@
 import numpy as np
 import onnxruntime as ort
 import argparse
+import onnx
+import io
 
 
 def print_numpy_array(arr):
-    print("arr: {}...".format(arr.reshape([-1]).tolist()[:6]))
+    arr = arr.reshape([-1]).tolist()[:6]
+    arr_str_format = len(arr) * "{:.2f}, "
+    arr_str = arr_str_format.format(*arr)
+    print("%s ..." % arr_str[:-2])
     print(
-        "min: {:.2f} max: {:.2f} avg: {:.2f} var: {:.2f}".format(
-            np.min(arr), np.max(arr), np.mean(arr), np.var(arr)
-        )
+        "Min: %.2f, Max: %.2f, Avg: %.2f, Var: %.2f"
+        % (np.min(arr), np.max(arr), np.mean(arr), np.var(arr))
     )
 
 
+def load_model_and_set_outs(model_path, out_names):
+    model = onnx.load(model_path)
+
+    if out_names:
+
+        for _ in range(len(model.graph.output)):
+            model.graph.output.pop()
+
+        for out_name in out_names:
+            intermediate_tensor_name = out_name
+            intermediate_layer_value_info = onnx.helper.ValueInfoProto()
+            intermediate_layer_value_info.name = intermediate_tensor_name
+            model.graph.output.extend([intermediate_layer_value_info])
+
+    else:
+        pass
+
+    f = io.BytesIO()
+    onnx.save(model, f)
+    f.seek(0)
+
+    return f
+
+
 def run_model(args):
-    session = ort.InferenceSession(args.model_path, providers=["CPUExecutionProvider"])
-    in_names = [i.name for i in session.get_inputs()]
+    model_file = load_model_and_set_outs(args.model_path, args.outs)
+    session = ort.InferenceSession(
+        model_file.read(), providers=["CPUExecutionProvider"]
+    )
+
     out_names = [i.name for i in session.get_outputs()]
 
-    ins_numpy = np.load(args.ins_file)
-    feeds = {k: ins_numpy.get(k) for k in in_names}
-    out_numpy = session.run(out_names, feeds)
+    numpy_ins = np.load(args.ins_file)
+    feeds = {k: numpy_ins.get(k.name) for k in session.get_inputs()}
 
-    for name, arr in zip(out_names, out_numpy):
-        print("\n[", name, "]")
-        print_numpy_array(arr)
+    numpy_outs = session.run(out_names, feeds)
+
+    for out_name, numpy_out in zip(out_names, numpy_outs):
+        print("\033[92m[%s]\033[0m" % out_name)
+        try:
+            print_numpy_array(numpy_out)
+        except:
+            print(numpy_out)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", required=True)
-    parser.add_argument("--ins-file", required=True)
+    parser.add_argument("--ins-file", required=True, help="Numpy Npz File")
+    parser.add_argument("--outs", nargs="+", type=str)
     args = parser.parse_args()
     run_model(args)
--- ml-ai/tensorflow/run-savedmodel.py
+++ ml-ai/tensorflow/run-savedmodel.py
@@ -3,23 +3,44 @@ import numpy as np
 import argparse
 
 
+def print_numpy_array(arr):
+    arr = arr.reshape([-1]).tolist()[:6]
+    arr_str_format = len(arr) * "{:.2f}, "
+    arr_str = arr_str_format.format(*arr)
+    print("%s ..." % arr_str[:-2])
+    print(
+        "Min: %.2f, Max: %.2f, Avg: %.2f, Var: %.2f"
+        % (np.min(arr), np.max(arr), np.mean(arr), np.var(arr))
+    )
+
+
 def run_savedmodel(args):
     with tf.Session(graph=tf.Graph()) as sess:
-        tf.saved_model.loader.load(sess, ["serve"], args.model_path)
+        metagraph = tf.saved_model.loader.load(sess, ["serve"], args.model_path)
+        outputs_mapping = dict(metagraph.signature_def["serving_default"].outputs)
+
+        out_names = (
+            [v.name for k, v in outputs_mapping.items()] if not args.outs else args.outs
+        )
 
         npz_data = np.load(args.ins_file)
-        ins_numpy = {in_name + ":0": npz_data[in_name] for in_name in npz_data.files}
+        numpy_ins = {in_name + ":0": npz_data[in_name] for in_name in npz_data.files}
 
         graph = tf.get_default_graph()
-        out_names = args.outs
         outs = [graph.get_tensor_by_name(out_name) for out_name in out_names]
-        print(sess.run(outs, feed_dict=ins_numpy))
+        numpy_outs = sess.run(outs, feed_dict=numpy_ins)
+        for out_name, numpy_out in zip(out_names, numpy_outs):
+            print("\033[92m[%s]\033[0m" % out_name)
+            try:
+                print_numpy_array(numpy_out)
+            except:
+                print(numpy_out)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", required=True)
-    parser.add_argument("--ins-file", required=True)
+    parser.add_argument("--ins-file", required=True, help="Numpy Npz File")
     parser.add_argument("--outs", nargs="+", type=str)
     args = parser.parse_args()
     run_savedmodel(args)
